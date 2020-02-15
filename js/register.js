296b9783029e81daebff6e602bbf014291c60274
张玥 <13081475269@163.com>
2020-02-15 20:48:13 +0800

    修改footer引用

---
+++ js/register.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.introduce').load("footer.html");
+});
+// $('.from').Validform({
+// 	tiptype: function (msg,o,cssctl) {
+// 		var $err=o.obj.parents('.input_box').siblings('.errorTip').find('.errorFont');
+// 		console.log($err.text())
+// 	}
+// });
