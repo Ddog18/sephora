c5c342a3ee4ec7d86e7d835d3394c5bf78729118
张玥 <13081475269@163.com>
2020-02-19 02:17:26 +0800

    新增user相关页面和js

---
+++ js/user_orderonline.js
@@ -0,0 +1,33 @@
+//nav_list 
+$('.nav_link').on('click','li',function(){
+	$('p').removeClass('now');
+	var flag=$(this).find('p').hasClass('now');
+	if(!flag){
+		$(this).find('p').addClass('now')
+		
+	}else{
+		$(this).find('p').removeClass('now')
+	}
+});
+
+$('table').on('click','.time',function(){
+	var flag=$(this).find('.order_triangle').hasClass('transform');
+	console.log(flag)
+	if(!flag){
+		$(this).find('.order_triangle').addClass('transform');
+		$(this).find('.time_list').show();
+	}else{
+		$(this).find('.order_triangle').removeClass('transform');
+		$(this).find('.time_list').hide();
+	}
+});
+$('table').on('click','.state',function(){
+	var flag=$(this).find('.order_triangle').hasClass('transform');
+	if(!flag){
+		$(this).find('.order_triangle').addClass('transform');
+		$(this).find('.state_list').show();
+	}else{
+		$(this).find('.order_triangle').removeClass('transform');
+		$(this).find('.state_list').hide();
+	}
+});
