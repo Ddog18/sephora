5b6b03568cc7db56d5fc77016fcf09ba1c9ca4a8
张玥 <13081475269@163.com>
2020-02-19 21:10:00 +0800

    新增用户信息修改页面

---
+++ js/user_infor.js
@@ -0,0 +1,17 @@
+$('.user_img').on('click','.recom_title',function () {
+	$(this).addClass('white');
+	$(this).siblings('.upload_title').removeClass('white');
+	$(this).siblings('.upload_title').addClass('black');
+	$(this).parents('.title').siblings('.recommend').show();
+	$(this).parents('.title').siblings('.upload').hide();
+})
+$('.user_img').on('click','.upload_title',function () {
+	$(this).addClass('white');
+	$(this).siblings('.recom_title').removeClass('white');
+	$(this).siblings('recom_title').addClass('black');
+	$(this).parents('.title').siblings('.recommend').hide();
+	$(this).parents('.title').siblings('.upload').show();
+})
+$('#btn').click(function () {
+	alert('保存成功');
+})
