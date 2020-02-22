87061f963548525acad234342b23bd09ec73c496
李诚甲 <1009387751@qq.com>
2020-02-22 10:35:16 +0800

    修改bug

--- js/register.js
+++ js/register.js
@@ -1,9 +1,4 @@
 $(function(){
 	$('.introduce').load("footer.html");
 });
-// $('.from').Validform({
-// 	tiptype: function (msg,o,cssctl) {
-// 		var $err=o.obj.parents('.input_box').siblings('.errorTip').find('.errorFont');
-// 		console.log($err.text())
-// 	}
-// });
+
