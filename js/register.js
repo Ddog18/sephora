299cf23f326cf7584f42303e4b860b67daae9482
张玥 <13081475269@163.com>
2020-02-22 16:15:14 +0800

    修改登录校验和验证注册的账号 验证成功跳转到首页

--- js/register.js
+++ js/register.js
@@ -1,4 +1,35 @@
-$(function(){
+$(function() {
 	$('.introduce').load("footer.html");
 });
+$('.form').Validform({
+	tiptype: function(msg, o, cssctl) {
+		if (!o.obj.is("form")) {
+			var objtip = o.obj.parent('.input_box').siblings('.errorTip ').children(".errorRedIcon");
+			var red = o.obj.siblings('.errorRed ');
+			var redline = o.obj.siblings('.errorRedLine ');
+			var text = o.obj.parent('.input_box').siblings('.errorTip ').children(".errorFont ");
+			if (o.type != 2) {
+				objtip.css('opacity', '1');
+				red.css('display', 'block');
+				redline.css('display', 'block');
+				text.text(msg);
+			} else {
+				text.text('');
+				objtip.css('opacity', '0');
+				red.css('display', 'none');
+				redline.css('display', 'none');
+			}
+			console.log(o.obj.is("form"))
 
+		} else {
+
+		}
+	},
+	beforeSubmit: function(curform) {
+		var username = $('#tel').val();
+		var userpass = $('#pass1').val();
+		document.cookie = "username=" + username;
+		document.cookie = "userpass=" + userpass;
+	}
+
+});
